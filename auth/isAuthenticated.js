8e3d23b0fb9f3724431df01a426c518e0711254c
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2025-02-11 11:52:01 +0600

    HOTFIX:/ Redirect to root

--- auth/isAuthenticated.js
+++ auth/isAuthenticated.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.status(401).json({ error: "Unauthorized! Please log in." });
-    // res.sendFile(__dirname + "../public/login.html");
+    
+    res.redirect("/");
 };
