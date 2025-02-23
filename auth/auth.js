c0204a1ec7b5d2a1e3efc542b11755cf32b4de2b
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2025-02-23 20:56:12 +0600

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -7,7 +7,8 @@ const db = admin.firestore();
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/google/callback",
+        // callbackURL: "http://localhost:3000/google/callback",
+        callbackURL: "https://monthly-time-calculator.onrender.com/google/callback",
         passReqToCallback : true
     },
     async function(request, accessToken, refreshToken, profile, done) {
